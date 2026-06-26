66bf974e42e8442a4bec6ae9cdc799ef8bd4c2e7
Mohddanish01 <mohddanish1729@gmail.com>
2026-06-26 23:13:05 +0530

    Implemented toilet management, reviews and demand voting

--- frontend/src/components/DemandCard.jsx
+++ frontend/src/components/DemandCard.jsx
@@ -1,26 +1,52 @@
+import api from "../api/axios";
+import { useState } from "react";
+
 function DemandCard({ demand }) {
+
+  const [votes, setVotes] = useState(demand.votes);
+
+  const handleVote = async () => {
+
+    try {
+
+      const res = await api.post(
+        `/demands/${demand._id}/vote`
+      );
+
+      setVotes(res.data.votes);
+
+    } catch (error) {
+
+      console.log(error);
+
+      alert(
+        error.response?.data?.message ||
+        "Failed to vote"
+      );
+    }
+  };
 
   return (
 
     <div>
 
       <p>
-        Votes: {demand.votes}
+        Votes: {votes}
       </p>
 
       <p>
         Lat:
-        {
-          demand.location.coordinates[1]
-        }
+        {demand.location.coordinates[1]}
       </p>
 
       <p>
         Lng:
-        {
-          demand.location.coordinates[0]
-        }
+        {demand.location.coordinates[0]}
       </p>
+
+      <button onClick={handleVote}>
+        Vote
+      </button>
 
       <hr />
 
