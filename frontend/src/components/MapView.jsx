8c88c92974ca4f2cfd3955068ba26c15de100e0d
Mohddanish01 <mohddanish1729@gmail.com>
2026-06-29 13:15:44 +0530

    Integrated Leaflet maps with custom markers

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -10,6 +10,27 @@ import {
 
 import { Link } from "react-router-dom";
 
+import L from "leaflet";
+import "leaflet.awesome-markers";
+
+const userIcon = L.AwesomeMarkers.icon({
+  icon: "user",
+  prefix: "fa",
+  markerColor: "green"
+});
+
+const toiletIcon = L.AwesomeMarkers.icon({
+  icon: "restroom",
+  prefix: "fa",
+  markerColor: "blue"
+});
+
+const demandIcon = L.AwesomeMarkers.icon({
+  icon: "flag",
+  prefix: "fa",
+  markerColor: "red"
+});
+
 function ChangeMapView({ center }) {
 
   const map = useMap();
@@ -55,112 +76,142 @@ function MapView({ toilets, demands }) {
 
   return (
 
-    <MapContainer  // current location show krega
-      // center={[28.6139, 77.2090]}
-      center={position}
-      zoom={13}
+    <div>
+      <MapContainer  // current location show krega
+        // center={[28.6139, 77.2090]}
+        center={position}
+        zoom={13}
+        style={{
+          height: "500px",
+          width: "100%"
+        }}
+      >
+
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <ChangeMapView
+          center={position}
+        />
+
+        <Marker
+          position={position}
+          icon={userIcon}
+        >
+
+          <Popup>
+
+            You are here
+
+          </Popup>
+
+        </Marker>
+
+        {  // toilet show krega
+          toilets.map((toilet) => (
+
+          <Marker
+            key={toilet._id}
+            position={[
+              toilet.location.coordinates[1],
+              toilet.location.coordinates[0]
+            ]}
+            icon={toiletIcon}
+          >
+          <Popup>
+
+            <h3>
+              {toilet.name}
+            </h3>
+
+            <p>
+              📍 {toilet.address}
+            </p>
+
+            <p>
+              ⭐ Average Rating:
+              {toilet.avg_rating.toFixed(1)}
+            </p>
+
+            <p>
+              📝 Reviews:
+              {toilet.total_reviews}
+            </p>
+
+            <Link
+              to={`/toilet/${toilet._id}`}
+            >
+              View Details
+            </Link>
+
+          </Popup>
+
+          </Marker>
+
+        ))
+      }
+
+      {  // demand show krega
+        demands.map((demand) => (
+
+          <Marker
+            key={demand._id}
+            position={[
+              demand.location.coordinates[1],
+              demand.location.coordinates[0]
+            ]}
+            icon={demandIcon}
+          >
+
+          <Popup>
+
+            <h3>
+              🚧 Toilet Needed
+            </h3>
+
+            <p>
+              👍 Votes:
+              {demand.votes}
+            </p>
+
+            <p>
+              Help this area get
+              a public toilet.
+            </p>
+
+          </Popup>
+
+          </Marker>
+
+        ))
+      }
+
+    </MapContainer>
+
+    <div
       style={{
-        height: "500px",
-        width: "100%"
+        background: "white",
+        padding: "10px",
+        borderRadius: "8px",
+        marginTop: "10px",
+        width: "250px",
+        boxShadow: "0 2px 8px rgba(0,0,0,0.2)"
       }}
     >
 
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <h3>Map Legend</h3>
 
-      <ChangeMapView
-        center={position}
-      />
+      <p>🟢 Your Location</p>
 
-      <Marker position={position}>
+      <p>🔵 Public Toilet</p>
 
-        <Popup>
+      <p>🔴 Toilet Demand</p>
 
-          You are here
+    </div>
 
-        </Popup>
+  </div>
 
-      </Marker>
-
-      {  // toilet show krega
-        toilets.map((toilet) => (
-
-        <Marker
-          key={toilet._id}
-          position={[
-            toilet.location.coordinates[1],
-            toilet.location.coordinates[0]
-          ]}
-        >
-        <Popup>
-
-          <h3>
-            {toilet.name}
-          </h3>
-
-          <p>
-            📍 {toilet.address}
-          </p>
-
-          <p>
-            ⭐ Average Rating:
-            {toilet.avg_rating.toFixed(1)}
-          </p>
-
-          <p>
-            📝 Reviews:
-            {toilet.total_reviews}
-          </p>
-
-          <Link
-            to={`/toilet/${toilet._id}`}
-          >
-            View Details
-          </Link>
-
-        </Popup>
-
-        </Marker>
-
-      ))
-    }
-
-    {  // demand show krega
-      demands.map((demand) => (
-
-        <Marker
-          key={demand._id}
-          position={[
-            demand.location.coordinates[1],
-            demand.location.coordinates[0]
-          ]}
-        >
-
-        <Popup>
-
-          <h3>
-            🚧 Toilet Needed
-          </h3>
-
-          <p>
-            👍 Votes:
-            {demand.votes}
-          </p>
-
-          <p>
-            Help this area get
-            a public toilet.
-          </p>
-
-        </Popup>
-
-        </Marker>
-
-      ))
-    }
-
-    </MapContainer>
 
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from "./context/AuthContext";
 import "leaflet/dist/leaflet.css";
+import "leaflet.awesome-markers/dist/leaflet.awesome-markers.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
